6a757df6feea1a2feb76219c8e00cfc3fc3e7f60
Thiago Cardoso <thiago.moura.cardoso@gmail.com>
2025-12-10 20:22:58 -0300

    Este foi meu primeiro contato com Spring Boot

---
+++ src/test/java/com/example/primeiro_spring/PrimeiroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primeiro_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
